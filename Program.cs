079f346db5936e86ed0448a34cd54967d5c5220f
user <Klimovich.maks@gmail.com>
2022-10-31 11:05:09 +0300

    add first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+
+int Max(int num)
+{
+    int firstValue = num / 10;
+    int secondValue = num % 100;
+    if (firstValue == secondValue) return -1;
+        return firstValue > secondValue ? firstValue : secondValue;
+}
+
+
+int number = new Random().Next(100, 1000);
+int max = Max(number);
+
+string result = max != -1 ? max.ToString() : "числа равны";
+Console.WriteLine($"Наибольшее цифра из {number} => {result}");
+
+
+
+
