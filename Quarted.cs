88983558100dbc63a4ff52a38d117c63a21d994e
user <Klimovich.maks@gmail.com>
2022-11-01 13:42:02 +0300

    quartet

---
+++ Quarted.cs
@@ -0,0 +1,12 @@
+
+string Quarted(int x, int y)
+{
+    if (x > 0 && y > 0) return "I quarted";
+    if (x < 0 && y > 0) return "II quarted";
+    if (x < 0 && y < 0) return "III quarted";
+    if (x > 0 && y < 0) return "IV quarted";
+    return "incorrected";
+}
+
+string quart = Quarted(-1,1);
+Console.WriteLine(quart);
